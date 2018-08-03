650653449b2407ff831077cf10647fed06c145b6
池田敬祐 <keisuke19980908@ikedakeisuke.local>
2018-08-03 15:36:42 +0900

    user definition1

---
+++ Student.java
@@ -0,0 +1,9 @@
+//g1744069 Keisuke Ikeda
+public class Student {
+    Integer id;
+    Double examScore;
+    Double assignmentScore;
+    Double miniexamScore;
+    Double totalScore;
+    String grade;
+}
